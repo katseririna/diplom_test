2c7c04ca15ab119fb04d9295ad52778852bb2865
toothl3ss <toothl3ss@Jessica.local>
2020-05-08 10:51:26 +0300

    fix save query

--- includes/db.php
+++ includes/db.php
@@ -45,7 +45,7 @@
     #---- Save realisation 
     #$sql = "SELECT * FROM person WHERE name = ? LIMIT 1 ";
     #$stmt = $conn->prepare($sql); 
-    #$stmt->bind_param("i", $id);
+    #$stmt->bind_param("si", $name);
     #$stmt->execute();
     #$result = $stmt->get_result();
     # while ($row = $stmt->fetch_assoc()) {
