a2f9846f779124aa7bda92c7ec97584b005d4850
toothl3ss <toothl3ss@Jessica.local>
2020-05-08 13:51:57 +0300

    fix save query1

--- includes/db.php
+++ includes/db.php
@@ -45,7 +45,12 @@
     #---- Save realisation 
     #$sql = "SELECT * FROM person WHERE name = ? LIMIT 1 ";
     #$stmt = $conn->prepare($sql); 
-    #$stmt->bind_param("s", $name);
+    #if (!$stmt->bind_param("s", $name)){
+    # echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+    #   }
+    #if (!$stmt->execute()) {
+    #echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+    #                        }
     #$stmt->execute();
     #$result = $stmt->get_result();
     # while ($row = $stmt->fetch_assoc()) {
